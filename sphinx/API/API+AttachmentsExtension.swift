163654efcdf2b759cdc53c78d2662cf1f34c3e1f
Jim Carucci <96802642+uncleJim21@users.noreply.github.com>
2024-05-10 14:49:15 -0500

    remove old sendAttachment API

--- sphinx/API/API+AttachmentsExtension.swift
+++ sphinx/API/API+AttachmentsExtension.swift
@@ -134,32 +134,6 @@ extension API {
         }
     }
     
-    public func sendAttachment(
-        params: [String : AnyObject],
-        callback: @escaping MessageObjectCallback,
-        errorCallback: @escaping EmptyCallback
-    ) {
-        guard let request = getURLRequest(route: "/attachment", params: params as NSDictionary?, method: "POST") else {
-            errorCallback()
-            return
-        }
-        
-        sphinxRequest(request) { response in
-            switch response.result {
-            case .success(let data):
-                if let json = data as? NSDictionary {
-                    if let success = json["success"] as? Bool, let response = json["response"] as? NSDictionary, success {
-                        callback(JSON(response))
-                    } else {
-                        errorCallback()
-                    }
-                }
-            case .failure(_):
-                errorCallback()
-            }
-        }
-    }
-    
     public func payAttachment(params: [String : AnyObject], callback: @escaping MessageObjectCallback, errorCallback: @escaping EmptyCallback) {
         guard let request = getURLRequest(route: "/purchase", params: params as NSDictionary?, method: "POST") else {
             errorCallback()
