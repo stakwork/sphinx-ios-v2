dee67780a1e9dbbf45d6907a95e7961aa897c844
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2024-05-28 11:58:09 -0300

    Reconnect or fetchNewMsgs on pull to refresh

--- sphinx/Scenes/Dashboard/DashboardRootViewController+DashboardChatsListDelegate.swift
+++ sphinx/Scenes/Dashboard/DashboardRootViewController+DashboardChatsListDelegate.swift
@@ -17,7 +17,7 @@ extension DashboardRootViewController: DashboardChatsListDelegate {
         _ viewController: UIViewController,
         using refreshControl: UIRefreshControl
     ) {
-        ///Replace with SphinxOnionManager refresh
+        SphinxOnionManager.sharedInstance.connectToServer()
         refreshControl.endRefreshing()
     }
     
