82ef2e59331f1c32f27df611496b7a61d4505122
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2024-06-12 16:26:10 -0300

    - AsyncPay payload separated from topics array to be published first - New button retry connection with friend

--- sphinx/Crypter/SphinxOnionManager.swift
+++ sphinx/Crypter/SphinxOnionManager.swift
@@ -68,7 +68,7 @@ class SphinxOnionManager : NSObject {
         }
     }
     
-    var settledRRObjects: [RunReturn] = []
+    var delayedRRObjects: [RunReturn] = []
     
     var msgTotalCounts : MsgTotalCounts? = nil
     
