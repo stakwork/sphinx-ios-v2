f7963f8c1994d435ad73e7cfdee8f33f9c3fbbb4
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2024-06-14 15:41:37 -0300

    Live updates unavailable alert commented

--- sphinx/Scenes/Dashboard/DashboardRootViewController.swift
+++ sphinx/Scenes/Dashboard/DashboardRootViewController.swift
@@ -360,11 +360,10 @@ extension DashboardRootViewController {
     }
 
     @objc private func didDisconnectFromInternet() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
-            AlertHelper.showAlert(title: "socket.disconnected".localized, message: "")
-        })
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+//            AlertHelper.showAlert(title: "socket.disconnected".localized, message: "")
+//        })
     }
-
     
     func hideRestoreViewCallback(){
         restoreProgressView.hideViewAnimated()
