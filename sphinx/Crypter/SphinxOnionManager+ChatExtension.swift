6aa87e7a6ffa1c21b2d3bdbe8227ea1c821a2973
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2024-08-02 18:21:39 -0300

    Signing fix

--- sphinx/Crypter/SphinxOnionManager+ChatExtension.swift
+++ sphinx/Crypter/SphinxOnionManager+ChatExtension.swift
@@ -1226,7 +1226,7 @@ extension SphinxOnionManager {
                 )
                 
                 let resultBase64 = Data(hexString: resultHex)?
-                    .base64EncodedString().urlSNotafe
+                    .base64EncodedString().urlSafe
                 
                 return resultBase64
             }
