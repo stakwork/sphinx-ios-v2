d53527ec06c31bd62d8270b30f08b70dd81ad0be
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2024-08-13 13:24:07 -0300

    Fix for network status

--- sphinx/NetworkMonitor.swift
+++ sphinx/NetworkMonitor.swift
@@ -1,3 +1,11 @@
+//
+//  NetworkMonitor.swift
+//  sphinx
+//
+//  Created by James Carucci on 6/10/24.
+//  Copyright © 2024 sphinx. All rights reserved.
+//
+
 import Foundation
 import Network
 
@@ -8,7 +16,6 @@ class NetworkMonitor {
     
     private(set) var isConnected: Bool = false
     var connectionType: NWInterface.InterfaceType?
-    var firstRun : Bool = true
     
     private init() {}
 
@@ -39,14 +46,7 @@ class NetworkMonitor {
 
     // Use SCNetworkReachability to determine the actual network state
     private func updateConnectionStatus(path: NWPath) {
-        if(firstRun){//compensate for iOS quirks!
-            isConnected = path.status == .satisfied
-            firstRun = false
-        }
-        else{
-            isConnected = path.status != .satisfied
-        }
-        
+        isConnected = path.status == .satisfied
         
         // Determine the connection type
         if path.usesInterfaceType(.wifi) {
@@ -69,8 +69,8 @@ class NetworkMonitor {
         }
     }
 
-    func checkConnectionSync() -> Bool {
-        guard let monitor = nwMonitor else { return false }
+    func isNetworkConnected() -> Bool {
+        guard let _ = nwMonitor else { return false }
         return isConnected
     }
 }
