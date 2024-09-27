1c4ff7bc0a56a285f177e4b31439fc29163c636a
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2024-09-27 15:04:58 -0300

    Fix on people authentication process

--- sphinx/API/API+PeopleExtension.swift
+++ sphinx/API/API+PeopleExtension.swift
@@ -20,6 +20,7 @@ extension API {
     typealias GetPersonProfileCallback = ((Bool, JSON?) -> ())
     typealias GetTribeMemberProfileCallback = ((Bool, TribeMemberStruct?) -> ())
     typealias SearchBTGatewayCallback = (([BTFeedSearchDataMapper]) -> ())
+    typealias CheckPeopleProfile = (Bool) -> ()
     
     public func authorizeBTGateway(
         url: String,
@@ -235,6 +236,65 @@ extension API {
                 callback(false, nil)
             case .failure(_):
                 callback(false, nil)
+            }
+        }
+    }
+    
+    public func checkPeopleProfileWith(
+        publicKey: String,
+        callback: @escaping CheckPeopleProfile
+    ) {
+        let url = "\(API.tribesV1Url)/person/\(publicKey)"
+        
+        guard let request = createRequest(url, bodyParams: nil, method: "GET") else {
+            callback(false)
+            return
+        }
+        
+        sphinxRequest(request) { response in
+            if let data = response.data {
+                let jsonProfile = JSON(data)
+                if let pubKey = jsonProfile["owner_pubkey"].string, pubKey == publicKey {
+                    callback(true)
+                } else {
+                    callback(false)
+                }
+            } else {
+                callback(false)
+            }
+        }
+    }
+    
+    public func createPeopleProfileWith(
+        token: String,
+        alias: String,
+        publicKey: String,
+        routeHint: String,
+        callback: @escaping CheckPeopleProfile
+    ) {
+        let url = "\(API.tribesV1Url)/person?token=\(token)"
+        
+        let params: [String: AnyObject] = [
+            "owner_pubkey": publicKey as AnyObject,
+            "owner_alias": alias as AnyObject,
+            "owner_route_hint": routeHint as AnyObject
+        ]
+        
+        guard let request = createRequest(url, bodyParams: params as NSDictionary, method: "POST") else {
+            callback(false)
+            return
+        }
+        
+        sphinxRequest(request) { response in
+            if let data = response.data {
+                let jsonProfile = JSON(data)
+                if let pubKey = jsonProfile["owner_pubkey"].string, pubKey == publicKey {
+                    callback(true)
+                } else {
+                    callback(false)
+                }
+            } else {
+                callback(false)
             }
         }
     }
