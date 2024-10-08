bc5d363359ff965fb47ff5ab99293873ccc82b86
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2024-10-08 11:16:24 -0300

    Fixes for UI blocked when typing new message

--- sphinx/Scenes/Chat/Data Sources/ChatMentionAutocompleteDataSource.swift
+++ sphinx/Scenes/Chat/Data Sources/ChatMentionAutocompleteDataSource.swift
@@ -45,42 +45,47 @@ class ChatMentionAutocompleteDataSource : NSObject {
     }
     
     func updateMentionSuggestions(suggestions: [(String, String)]){
-        tableView.isHidden = (suggestions.isEmpty == true)
-        
-        let suggestionObjects = suggestions.compactMap({
-            let result = MentionOrMacroItem(
-                type: .mention,
-                displayText: $0.0,
-                imageLink: $0.1,
-                action: nil
-            )
-            return result as! MentionOrMacroItem
-        }) as? [MentionOrMacroItem]
-        mentionSuggestions = suggestionObjects ?? []
-        
-        tableView.reloadData()
-        
-        if (suggestions.isEmpty == false) {
-            let bottom = IndexPath(
-                row: 0,
-                section: 0
-            )
-            tableView.scrollToRow(at: bottom, at: .bottom, animated: true)
+        DispatchQueue.main.async {
+            self.tableView.isHidden = (suggestions.isEmpty == true)
+            
+            let suggestionObjects = suggestions.compactMap({
+                let result = MentionOrMacroItem(
+                    type: .mention,
+                    displayText: $0.0,
+                    imageLink: $0.1,
+                    action: nil
+                )
+                return result
+            }) as? [MentionOrMacroItem]
+            
+            self.mentionSuggestions = suggestionObjects ?? []
+            
+            self.tableView.reloadData()
+            
+            if (suggestions.isEmpty == false) {
+                let bottom = IndexPath(
+                    row: 0,
+                    section: 0
+                )
+                self.tableView.scrollToRow(at: bottom, at: .bottom, animated: true)
+            }
         }
     }
     
-    func updateMacroSuggestions(macros:[MentionOrMacroItem]){
-        tableView.isHidden = (macros.isEmpty == true)
-        mentionSuggestions = macros
-        
-        tableView.reloadData()
-        
-        if (macros.isEmpty == false) {
-            let bottom = IndexPath(
-                row: 0,
-                section: 0
-            )
-            tableView.scrollToRow(at: bottom, at: .bottom, animated: true)
+    func updateMacroSuggestions(macros: [MentionOrMacroItem]) {
+        DispatchQueue.main.async {
+            self.tableView.isHidden = (macros.isEmpty == true)
+            self.mentionSuggestions = macros
+            
+            self.tableView.reloadData()
+            
+            if (macros.isEmpty == false) {
+                let bottom = IndexPath(
+                    row: 0,
+                    section: 0
+                )
+                self.tableView.scrollToRow(at: bottom, at: .bottom, animated: true)
+            }
         }
     }
     
