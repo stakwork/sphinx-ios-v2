33b4bb3cc2fa33c3499430d1835a3aefb7c8d878
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2025-01-21 14:47:33 -0300

    Notify all participants when call is being recorded

--- sphinx/Scenes/Chat/View Controllers/Like Kit/Views/RoomView.swift
+++ sphinx/Scenes/Chat/View Controllers/Like Kit/Views/RoomView.swift
@@ -51,7 +51,7 @@ struct RoomView: View {
     
     @State var isAnyParticipantAudioSubscribed = true
     
-    @State private var isRecording = false
+    @State private var didStartRecording = false
     @State private var isProcessingRecordRequest = false
     @State private var shouldAnimate = false
     
@@ -70,10 +70,10 @@ struct RoomView: View {
         }
         isProcessingRecordRequest = true
         
-        let urlAction = isRecording ? "stop" : "start"
+        let urlAction = didStartRecording ? "stop" : "start"
         var isoStringWithMilliseconds: String? = nil
         
-        if !isRecording {
+        if !didStartRecording {
             let currentDate = Date()
             let isoFormatter = ISO8601DateFormatter()
             isoFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
@@ -86,16 +86,17 @@ struct RoomView: View {
             action: urlAction,
             callback: { success in
                 if success {
-                    self.newMessageBubbleHelper.showGenericMessageView(
-                        text: self.isRecording ? "Recording ended. This call is no longer being recorded." : "Recording in progress. Please be aware this call is being recorded.",
-                        delay: 5,
-                        textColor: UIColor.white,
-                        backColor: UIColor.Sphinx.BadgeRed,
-                        backAlpha: 1.0
-                    )
-                    
-                    self.isRecording = !self.isRecording
-                    self.shouldAnimate = !self.shouldAnimate
+                    DispatchQueue.main.async {
+                        self.didStartRecording = !self.didStartRecording
+                        
+                        self.newMessageBubbleHelper.showGenericMessageView(
+                            text: self.didStartRecording ? "Starting call recording. Please wait..." : "Stopping call recording. Please wait...",
+                            delay: 5,
+                            textColor: UIColor.white,
+                            backColor: UIColor.Sphinx.BadgeRed,
+                            backAlpha: 1.0
+                        )
+                    }
                 }
                 self.isProcessingRecordRequest = false
             }
@@ -151,7 +152,7 @@ struct RoomView: View {
                         
                         Spacer()
                         
-                        if isRecording {
+                        if didStartRecording || room.isRecording {
                             Image(systemSymbol: .recordCircle)
                                 .renderingMode(.template)
                                 .foregroundColor(Color(UIColor.Sphinx.BadgeRed))
@@ -164,7 +165,7 @@ struct RoomView: View {
                                 }
                                 .onDisappear {
                                     shouldAnimate = false
-                                    isRecording = false
+                                    didStartRecording = false
                                 }
                         }
                         
@@ -228,7 +229,7 @@ struct RoomView: View {
                                 }
                             }
                             
-                            if !isProcessingRecordRequest {
+                            if room.isRecording && didStartRecording && !isProcessingRecordRequest {
                                 Group {
                                     Divider()
                                     
@@ -237,7 +238,19 @@ struct RoomView: View {
                                             toggleRecording()
                                         }
                                     } label: {
-                                        Text(isRecording ? "Stop Recording" : "Start Recording")
+                                        Text("Stop Recording")
+                                    }
+                                }
+                            } else if !room.isRecording && !isProcessingRecordRequest {
+                                Group {
+                                    Divider()
+                                    
+                                    Button {
+                                        Task { @MainActor in
+                                            toggleRecording()
+                                        }
+                                    } label: {
+                                        Text("Start Recording")
                                     }
                                 }
                             }
@@ -546,7 +559,7 @@ struct RoomView: View {
                             GeometryReader { geometry in
                                 Button(action: {
                                     Task {
-                                        if (isRecording) {
+                                        if didStartRecording && room.isRecording {
                                             toggleRecording()
                                         }
                                         await roomCtx.disconnect()
@@ -850,6 +863,17 @@ struct RoomView: View {
                     }
                 }
             }
+        }
+        .onChange(of: room.isRecording) { newValue in
+            self.newMessageBubbleHelper.showGenericMessageView(
+                text: newValue ? "Recording in progress.\nPlease be aware this call is being recorded." : "Recording ended.\nThis call is no longer being recorded.",
+                delay: 5,
+                textColor: UIColor.white,
+                backColor: UIColor.Sphinx.BadgeRed,
+                backAlpha: 1.0
+            )
+            
+            self.shouldAnimate = newValue
         }
     }
 }
