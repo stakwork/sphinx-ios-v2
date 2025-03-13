2044400b7affbe4ec42f0a90657af87b5c0a7be4
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2025-03-13 10:39:51 -0300

    Skip ads button implemented

--- sphinx/Scenes/WebAppsAndPodcasts/Views/EpisodesHeaderView.swift
+++ sphinx/Scenes/WebAppsAndPodcasts/Views/EpisodesHeaderView.swift
@@ -13,6 +13,20 @@ class EpisodesHeaderView: UIView {
     @IBOutlet var contentView: UIView!
     @IBOutlet weak var episodesLabel: UILabel!
     @IBOutlet weak var episodesCountLabel: UILabel!
+    @IBOutlet weak var skipAdsContainer: UIView!
+    @IBOutlet weak var skipAdsLabel: UILabel!
+    
+    public var skipAds : Bool {
+        get {
+            if let skipAds = UserDefaults.Keys.skipAds.get(defaultValue: false) {
+                return skipAds
+            }
+            return false
+        }
+        set {
+            UserDefaults.Keys.skipAds.set(newValue)
+        }
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,10 +43,26 @@ class EpisodesHeaderView: UIView {
         addSubview(contentView)
         contentView.frame = self.bounds
         contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        skipAdsContainer.layer.cornerRadius = 15
+        skipAdsContainer.layer.borderWidth = 1
     }
     
     func configureWith(count: Int) {
         episodesLabel.text = "episodes".localized.uppercased()
         episodesCountLabel.text = "\(count)"
+        
+        configureSkipAdsButton(enable: skipAds)
+    }
+    
+    @IBAction func skipAdsButtonTouched() {
+        let newValue = !skipAds
+        skipAds = newValue
+        configureSkipAdsButton(enable: newValue)
+    }
+    
+    func configureSkipAdsButton(enable: Bool) {
+        skipAdsContainer.backgroundColor = enable ? UIColor.Sphinx.PrimaryGreen : UIColor(hex: "#B0B7BC")
+        skipAdsLabel.textColor = enable ? UIColor.white : UIColor(hex: "#6B7A8D")
     }
 }
