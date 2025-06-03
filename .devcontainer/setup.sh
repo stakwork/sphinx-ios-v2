1746da605e3498900d3d5a2551cf59d8ea4f82c9
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2025-06-03 12:26:45 -0300

    Codespace files added

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /workspaces
+
+git clone https://github.com/stakwork/sphinx-android-v2.git
+git clone https://github.com/stakwork/kotlin-android.git
+git clone https://github.com/stakwork/sphinx-mac-v2.git
+git clone https://github.com/stakwork/sphinx-kotlin-v2.git
+git clone https://github.com/stakwork/sphinx-kotlin-core.git
