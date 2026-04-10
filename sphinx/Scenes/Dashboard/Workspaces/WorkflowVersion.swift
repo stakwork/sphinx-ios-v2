be161f6928c5f5e2f0776ab73690a3398289cb6e
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2026-04-10 19:54:24 -0300

    Parsing and sending workflow json as string

--- sphinx/Scenes/Dashboard/Workspaces/WorkflowVersion.swift
+++ sphinx/Scenes/Dashboard/Workspaces/WorkflowVersion.swift
@@ -15,7 +15,7 @@ struct WorkflowVersion: Sendable {
     let refId: String?          // ref_id (graph node ref)
     let workflowVersionId: String? // full UUID string
     let published: Bool
-    let workflowJson: [String: Any]?
+    let workflowJson: String?
 
     init?(json: JSON) {
         guard let versionId = json["workflow_version_id"].int,
@@ -26,6 +26,6 @@ struct WorkflowVersion: Sendable {
         self.refId = json["ref_id"].string
         self.workflowVersionId = json["ref_id"].string
         self.published = json["published"].bool ?? false
-        self.workflowJson = json["workflow_json"].dictionaryObject
+        self.workflowJson = json["workflow_json"].string
     }
 }
